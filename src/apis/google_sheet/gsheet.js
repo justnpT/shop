7aac7a5208af390936eef469931d2920575b1236
justnpTsii <mnowierski@sii.pl>
2019-09-06 19:27:48 +0200

    some progress

--- src/apis/google_sheet/gsheet.js
+++ src/apis/google_sheet/gsheet.js
@@ -4,17 +4,18 @@ var async = require('async');
 // spreadsheet key is the long id in the sheets URL
 var doc = new GoogleSpreadsheet('1R0pNdZ3JhVjYbfsT9z7EEkqlfwqsVgBEUfjQIf6gtV4');
 var sheet;
- 
+
+let first_col = 1;
+let last_col = 17;
+let captionRow = 4;
+// let itd = 'item data';
+// let prd = "product decisions";
+// let its = "item status information";
+// let ith = "item historical data";
+
 async.series([
   function setAuth(step) {
-    // see notes below for authentication instructions!
     var creds = require('./creds/shop-250916-e18ae184fd04.json');
-    // OR, if you cannot save the file locally (like on heroku)
-    // var creds_json = {
-    //   client_email: 'yourserviceaccountemailhere@google.com',
-    //   private_key: 'your long private key stuff here'
-    // }
- 
     doc.useServiceAccountAuth(creds, step);
   },
   function getInfoAndWorksheets(step) {
@@ -33,8 +34,10 @@ async.series([
       orderby: 'col2'
     }, function( err, rows ){
       console.log('Read '+rows.length+' rows');
-        console.log(rows[0])
-      
+        // console.log(rows[3])
+        // console.log(rows[4])
+        // console.log(rows[5])
+
       // the row is an object with keys set by the column headers
     //   rows[0].colname = 'new val';
     //   rows[0].save(); // this is async
@@ -47,11 +50,29 @@ async.series([
   },
   function workingWithCells(step) {
     sheet.getCells({
-      'min-row': 5,
-      'max-row': 17,
-      'return-empty': true
+      'min-col': first_col,
+      'max-col': last_col,
+      'min-row': captionRow,
+      'return-empty': false
     }, function(err, cells) {
-        for (const cell of cells) {
+
+      let captions = []
+      let items = []
+
+      let last_row = cells[cells.length-1]['row']
+      let firstA = cells.filter((value) => value['row'] == captionRow)
+
+      firstA.forEach((elem) => captions.push(elem['_value']))
+
+
+      for (let currentRow = captionRow+1; currentRow < last_row; currentRow++) {
+        let analyzedRow = cells.filter((value) => value['row'] == currentRow)
+        // TODO: zrobić tak aby powstało ->
+        // [{'name': 'szachy', 'tilte': 'super szachy', 'description': 'nowoczesne szachy'}, {}]
+        // TODO: sprawdzić czy ta struktura to jest to czego potrzebuje
+      }
+
+      for (const cell of cells) {
             // var cell = cells[0];
             console.log('Cell R'+cell.row+'C'+cell.col+' = '+cell.value);
         }
